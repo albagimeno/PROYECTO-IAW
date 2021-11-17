e88494c930cd2c375d98f82546525d65d10afee8
Userman <eduardoloes2002@gmail.com>
2021-11-17 13:52:21 +0100

    cambiarpwd

--- cambiar_pwd.sh
+++ cambiar_pwd.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
-echo -n "Elige un usuario:"
+read -p  "Introduce un usuario: " usuario
+if [$usuario = ""]
+then
+	echo "ERROR: Ponga un usuario"
+else
+	echo "Generando contraseña"
+	new_pwd=$(tr -dc 'a-zA-Z0-9.!\$' < /dev/random | head -c 8)
 
+	echo "Tu contraseña es: " $new_pwd
+	chpasswd $usuario:$new_pwd
+fi
