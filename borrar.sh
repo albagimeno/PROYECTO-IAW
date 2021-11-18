d938591bf30f772e823df32701d28691a038208f
Ahmed <ahmedmario@hotmail.es>
2021-11-18 13:28:03 +0100

    borrar

---
+++ borrar.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+read -p "introduzca un usuario: " usuario
+if  [ "$usuario" != "" ]
+then
+	userdel $usuario -r
+	a2dissite web_$usuario.conf
+	rm /etc/apache2/sites-available/web_$usuario.conf
+	systemctl reload apache2
+	echo "delete database $usuario" | mariadb
+else
+	echo "ERROR:no has introducido un usuario"
+fi
+./menu.sh
