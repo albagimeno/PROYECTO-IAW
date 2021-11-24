f5273af626b24a7d8c966dc5eff2b2f9b3e89dd7
albagimeno <albagm09@gmail.com>
2021-11-24 13:16:39 +0100

    alba

--- ALBA/menu.sh
+++ ALBA/menu.sh
@@ -6,18 +6,17 @@ read op
 echo -e "\n"
 
 if [ $op =1 ]; then
-    
     echo -e "\e[31m1. Listado de usuarios\e[0m"
     echo -e "\e[31m======================\e[0m"
-    ./listar_usuarios.sh
+    sudo su -c "./listar_usuarios.sh" root
 elif [ $op = 2 ]; then
     echo -e "\e[31m2. Crear usuarios\e[0m"
     echo -e "\e[31m=================\e[0m"
-    ./crear_usuarios.sh
+    sudo su -c "./crear_usuarios.sh" root
 elif [ $op = 3 ]; then
     echo -e "\e[31m2. Borrar usuario\e[0m"
     echo -e "\e[31m=================\e[0m"
-    ./borrar.sh
+    sudo su -c "./borrar.sh" root
 elif [ $op = 4 ]; then
     echo -e "\e[31m2. Deshabilitar usuario\e[0m"
     echo -e "\e[31m=================\e[0m"
