d63bb9411599628839744f5ccc1334a927066cfb
albagimeno <albagm09@gmail.com>
2021-11-24 13:26:00 +0100

    createuser

--- ALBA/crear_usuarios.sh
+++ ALBA/crear_usuarios.sh
@@ -67,8 +67,8 @@ echo "<VirtualHost *:80>
    CustomLog ${APACHE_LOG_DIR}/access.log combined
 </VirtualHost>" >> /etc/apache2/sites-available/blog_$nombre.conf
 
-wget https://wordpress.org/latest.tar.gz /var/www/$nombre/blog/
-tar xvzf latest.tar.gz /var/www/$nombre/blog/
+wget https://wordpress.org/latest.zip /var/www/$nombre/blog/
+unzip /var/www/$nombre/blog/latest.zip
 cp -r /var/www/$nombre/blog/wordpress/* /var/www/$nombre/blog/
 cp /var/www/$nombre/blog/wp-config-sample.php /var/www/$nombre/blog/wp-config.php
 
