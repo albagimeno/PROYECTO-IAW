7fc7a68b26f2f6538258747707c496d516eaa76f
albagimeno <albagm09@gmail.com>
2021-11-25 09:49:09 +0100

    createuser

--- ALBA/menu.sh
+++ ALBA/menu.sh
@@ -5,10 +5,10 @@ echo -n "Seleccione una opcion: "
 read op
 echo -e "\n"
 
-if [ $op =1 ]; then
+if [ $op = 1 ]; then
     echo -e "\e[31m1. Listado de usuarios\e[0m"
     echo -e "\e[31m======================\e[0m"
-    "./listar_usuarios.sh" root
+    sudo su -c "./listar_usuarios.sh" root
 elif [ $op = 2 ]; then
     echo -e "\e[31m2. Crear usuarios\e[0m"
     echo -e "\e[31m=================\e[0m"
