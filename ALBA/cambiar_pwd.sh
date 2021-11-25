e85bad39edd04bf650b513450418e2396d1b3685
albagimeno <albagm09@gmail.com>
2021-11-25 09:50:09 +0100

    createuser

--- ALBA/cambiar_pwd.sh
+++ ALBA/cambiar_pwd.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 read -p  "Introduce un usuario: " usuario
 
-while [ $? -eq 0 ]
-do
-    	echo "ERROR: Escriba de nuevo el nombre de usuario"
-    	echo -n "Escriba el nombre de usuario: "
-    	read usuario
-	egrep "$usuario" /etc/passwd >/dev/null
-done
-
 	echo "Generando contraseña"
 	new_pwd=$(< /dev/urandom tr -dc A-Za-z0-9 | head -c15; echo)
 
